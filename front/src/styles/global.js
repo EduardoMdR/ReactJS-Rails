296dd0a9862a5d8daeef2e2e46b1608614801813
Eduardo Marques dos Reis <54919290+EduardoMdR@users.noreply.github.com>
2021-07-02 15:07:28 -0300

    Initial Commit front

---
+++ front/src/styles/global.js
@@ -0,0 +1,22 @@
+import {createGlobalStyle} from 'styled-components'
+import Background from '../assets/background.jpg'
+
+export const GlobalStyle = createGlobalStyle`
+
+  :root {
+    --footer: #232323
+  }
+
+  * {
+    margin: 0;
+    padding: 0;
+    box-sizing: border-box;
+  }
+
+  body {
+    font-family: sans-serif;
+    background: url(${Background}) no-repeat center fixed;
+    background-size: cover;
+  }
+
+`;
