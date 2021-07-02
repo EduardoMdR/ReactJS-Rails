50c189a28439b4e917f6b9d465c1c38b919dcd8f
Eduardo Marques dos Reis <54919290+EduardoMdR@users.noreply.github.com>
2021-07-02 17:53:25 -0300

    Gem Cors & React Axios

--- front/src/App.js
+++ front/src/App.js
@@ -1,10 +1,22 @@
 import {GlobalStyle} from './styles/global'
+import { api } from './services/api.js'
+import { useState, useEffect } from 'react'
 
 function App() {
+  const [games, setGames] = useState([])
+
+  useEffect(() => {
+    api.get('games')
+    .then((response) => {setGames(response.data)})
+  }, [])
+
   return (
     <>
     <GlobalStyle />
     <h1>Gallery</h1>
+    <ul>
+      {games.map((jogo) => {return <li key={jogo.id}>{jogo.name}</li>})}
+    </ul>
     </>
   );
 }
