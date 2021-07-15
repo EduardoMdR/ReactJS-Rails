0ebee23b90dd45fafe9fb122107f657c3de8d6ac
Eduardo Marques dos Reis <54919290+EduardoMdR@users.noreply.github.com>
2021-07-14 21:46:07 -0300

    Genre show css

--- front/src/pages/GenreShow/index.js
+++ front/src/pages/GenreShow/index.js
@@ -1,27 +1,53 @@
-import { Container } from './styles.js'
+import { Container, GridMenu, GridGame, GameSection } from './styles.js'
 import { useState, useEffect } from 'react'
 import { api } from '../../services/api.js'
 import { Link } from 'react-router-dom'
+import { FiChevronsRight } from 'react-icons/fi';
 
 export function GenreShow({match}) {
 
   const [genre, setGenre] = useState([])
+  const [game_genre, setGameGenre] = useState([])
 
   useEffect(() => {
+    api.get('genres')
+    .then((response) => {setGenre(response.data)})
     api.get(`genres/${match.params.id}`)
-    .then((response) => {setGenre(response.data)}).catch((error) => {alert(error)})
-  }, [])
+    .then((response) => {setGameGenre(response.data)})
+  }, [game_genre])
 
   return (
     <>
       <Container>
-        <h2>Gênreo</h2>
-        {genre.map((gg) => 
-          {return <li key={gg.id}>
-            <Link to={`../games/${gg.game_id}`}>{gg.game_id}</Link>
-          </li>})
-        }
+        <GridMenu>
+          <h1>Explore seus gêneros favoritos</h1>
+          
+          {genre.map((genero) => {
+            return (
+              <p><FiChevronsRight size={14} strokeWidth={1} color="#DCDCDC" /><Link to={`./${genero.id}`} style={{color: '#fff', textDecoration: 'none' }}>{genero.name}</Link></p>
+            )
+          })}
+        </GridMenu>
+        <GridGame>
+        <Link to={`/games/1`} style={{ textDecoration: 'none' }}>
+          <GameSection>
+              <img src={'https://img.hype.games/cdn/facad932-4082-4d20-980d-34bb385d2233Red-Dead-Redemption-2-Ultimate-Edition-Cover.jpg'} alt='img' />
+              <span>RDR II</span>
+          </GameSection>
+          </Link>
+          <GameSection>
+            <img src={'https://s2.glbimg.com/I9MFCGPOKeCJbvS7qVFsVupgUiE=/0x0:1920x1080/984x0/smart/filters:strip_icc()/i.s3.glbimg.com/v1/AUTH_08fbf48bc0524877943fe86e43087e7a/internal_photos/bs/2017/n/q/Wqwk8kQueGABmPSAfawA/maxresdefault.jpg'} alt='img' />
+            <span>GTA IV</span>
+          </GameSection>
+          <GameSection>
+            <img src={'https://s2.glbimg.com/I9MFCGPOKeCJbvS7qVFsVupgUiE=/0x0:1920x1080/984x0/smart/filters:strip_icc()/i.s3.glbimg.com/v1/AUTH_08fbf48bc0524877943fe86e43087e7a/internal_photos/bs/2017/n/q/Wqwk8kQueGABmPSAfawA/maxresdefault.jpg'} alt='img' />
+            <span>GTA IV</span>
+          </GameSection>
+        </GridGame>
       </Container>
+      {game_genre.map((gg) => 
+          {return <p>{gg.game_id}</p>})
+        }
     </>
   );
 }
--- front/src/pages/GenreShow/styles.js
+++ front/src/pages/GenreShow/styles.js
@@ -1,4 +1,81 @@
 import styled from 'styled-components';
 
 export const Container = styled.section`
-`
+  display: flex;
+  height: 100%;
+  /* min-height: 93vh; */
+  background-color: #17202A;
+`;
+
+export const GridMenu = styled.div`
+  display: flex;
+  flex-direction: column;
+  flex-wrap: wrap;
+  margin: 1rem;
+  width: 30vh;
+  background:rgba(0,0,0,0.1);
+  border-radius: 10px;
+  h1 {
+    color: #ffffff;
+    font-size: 2rem;
+    margin-bottom: 1rem;
+    text-align: center;
+  }
+  p {
+    color: #ffffff;
+    margin: 0.1rem;
+    font-size: 0.9rem;
+  }
+`;
+
+export const GridGame = styled.div`
+  display: flex;
+  margin: 1rem;
+  flex-wrap: wrap;
+  width: 170vh; /* Why?????? */
+  justify-content: center;
+  background:rgba(0,0,0,0.3);
+  border-radius: 10px;
+`;
+
+export const GameSection = styled.div`
+  display: flex;
+  flex-direction: column;
+  margin: 0.1rem;
+  border-radius: 10px;
+  padding: 0.2rem;
+  background:rgba(0,0,0,0.1);
+  max-height: 125px;
+  height: 122px;
+  position: relative;
+  transition: 0.5s;
+  span {
+    font-size: 1.125rem;
+    color: #fff;
+    background:rgba(0,0,0,0.7);
+    width: 97%;
+    height: 1.7rem;
+    border-bottom-left-radius: 10px;
+    border-bottom-right-radius: 10px;
+    text-align: center;
+    position: absolute;
+    bottom: 0;
+    transition: 0.5s;
+  }  
+  &:hover {
+    background-color: #fff;
+    color: #000;
+    cursor: pointer;
+    span {
+      background-color: #fff;
+      color: #000;
+      cursor: pointer;
+    }
+  }
+
+  img {
+    width: 200px;
+    min-height: 125px;
+    border-radius: 10px;
+  }
+`;
