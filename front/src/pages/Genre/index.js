e7387d4ec7d309fbd8f778cded71c1ed92c0c59e
Eduardo Marques dos Reis <54919290+EduardoMdR@users.noreply.github.com>
2021-07-14 22:20:18 -0300

    Genre index css

--- front/src/pages/Genre/index.js
+++ front/src/pages/Genre/index.js
@@ -1,24 +1,38 @@
-import { Container } from './styles.js'
+import { Container, Grid, GenreSection } from './styles.js'
 import { useState, useEffect } from 'react'
 import { api } from '../../services/api.js'
 import { Link } from 'react-router-dom'
+import { FiChevronsRight } from 'react-icons/fi';
+import ACTION from '../../assets/genre_action.png'
 
-export const Genre = () => {
+export function Genre({match}) {
 
   const [genre, setGenre] = useState([])
+  const [game_genre, setGameGenre] = useState([])
 
   useEffect(() => {
     api.get('genres')
     .then((response) => {setGenre(response.data)})
+    api.get(`genres/${match.params.id}`)
+    .then((response) => {setGameGenre(response.data)})
   }, [])
 
   return (
     <>
       <Container>
-        <h2>Gêneros</h2>
-        <ul>
-          {genre.map((genero) => {return <li key={genero.id}><Link to={`genres/${genero.id}`}>{genero.name}</Link></li>})}
-        </ul>
+        <h1>Explore seus gêneros favoritos</h1>
+        <Grid>
+          {genre.map((genero) => {
+            return (
+              <Link to={`./genres/${genero.id}`} style={{color: '#fff', textDecoration: 'none' }}>
+                <GenreSection>
+                  <span>{genero.name}</span>
+                  <img src='https://cdn.akamai.steamstatic.com/store/promo/summer2021/genre_tiles/genre_tile_rpg.png?v=1' alt='img' />
+                </GenreSection>
+              </Link>
+            )
+          })}
+        </Grid>
       </Container>
     </>
   );
--- front/src/pages/Genre/styles.js
+++ front/src/pages/Genre/styles.js
@@ -1,4 +1,53 @@
 import styled from 'styled-components';
 
 export const Container = styled.section`
-`
+  display: flex;
+  height: 100%;
+  min-height: 100vh;
+  flex-direction: column;
+  background-color: #17202A;
+  h1 {
+    color: #ffffff;
+    font-size: 2rem;
+    margin-bottom: 3px;
+    text-align: center;
+    font-family: 'Otomanopee One', sans-serif;
+  }
+`;
+
+export const Grid = styled.div`
+  display: flex;
+  margin: 1rem;
+  flex-wrap: wrap;
+  justify-content: space-around;
+  background:rgba(0,0,0,0.1);
+  border-radius: 10px;
+  padding-bottom: 2rem;
+`;
+
+export const GenreSection = styled.div`
+  display: flex;
+  flex-direction: column;
+  margin: 0.2rem;
+  border-radius: 10px;
+  padding: 0.2rem;
+  transition: 0.3s;
+  span {
+    color: #ffffff;
+    font-size: 1.35rem;
+    margin-bottom: 3px;
+    text-align: center;
+    font-family: 'Orbitron', sans-serif;  }
+    transition: 0.2s;
+  img {
+    width: 175px;
+    border-radius: 10px;
+  }
+  &:hover {
+    background: #fff;
+    /* -webkit-text-stroke: 1px black; */
+    span {
+      color: #000;
+    }
+  }
+`;
